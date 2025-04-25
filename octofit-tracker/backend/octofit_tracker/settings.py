e87d0af17615fdf496ec4d5b54ed94ff6b85626e
Cintia Nunes <37228161+Cintiaa@users.noreply.github.com>
2025-04-25 02:36:06 +0000

    Add MongoDB configuration and enable CORS in Django settings

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -73,6 +73,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Configuração do banco de dados MongoDB
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
@@ -123,3 +124,26 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Configuração adicional para o projeto Django
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
+        'HOST': 'localhost',
+        'PORT': 27017,
+    }
+}
+
+# Habilitar CORS e permitir todas as origens
+INSTALLED_APPS += [
+    'corsheaders',
+]
+
+MIDDLEWARE += [
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_HOSTS = ['*']
